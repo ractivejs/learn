0fa37419eb6a75e5d6ab121b4952d963fba34c9a
Rich Harris <richard.a.harris@gmail.com>
2014-07-19 11:44:08 -0400

    updated more tutorials

--- src/tutorials/05/steps/2/console.js
+++
@@ -1,8 +0,0 @@
-var newSuperhero = {
-  name: 'Storm',
-  realname: 'Monroe, Ororo',
-  power: 'Controlling the weather',
-  info: 'http://www.superherodb.com/Storm/10-135/'
-};
-
-// add Storm to the list

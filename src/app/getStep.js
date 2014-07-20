538c014a1a381f6141f6908a86f5cc435ed2ba62
Rich Harris <richard.a.harris@gmail.com>
2014-07-20 18:17:17 -0400

    prefetch steps

--- src/app/getStep.js
+++ src/app/getStep.js
@@ -1,22 +1,54 @@
 import Ractive from 'ractive';
 import get from 'utils/get';
+import slugify from 'utils/slugify';
 
-var promises, baseUrl, step;
+var promises, baseUrl, data, step, indexByTitle = {}, queue = [];
 
 promises = {};
 baseUrl = window.location.origin;
 
 // we already have some data...
-step = window.TUTORIAL_DATA.step;
+data = window.TUTORIAL_DATA;
+step = data.step;
 promises[ `${baseUrl}/${step.tutorialTitle}/${step.index+1}/index.json` ] = Ractive.Promise.resolve( step );
 
-export default function fetch ( title, number ) {
-	var url = `${baseUrl}/${title}/${number}/index.json`;
+// So we can identify next/previous steps...
+data.manifest.forEach( ( x, i ) => {
+	var title = slugify( x.title );
+	indexByTitle[ title ] = i;
+	queue.push( title );
+});
+
+// Pre-fetch the first step of each tutorial
+fetchNext();
+
+export default fetch;
+
+function fetchNext () {
+	var title;
+
+	if ( title = queue.shift() ) {
+		fetch( title, 1 ).then( fetchNext );
+	}
+}
+
+function fetch ( title, number ) {
+	var url, promise, tutorialIndex, tutorialData;
+
+	url = `${baseUrl}/${title}/${number}/index.json`;
 
 	if ( !promises[ url ] ) {
-		promises[ url ] = get( url ).then( function ( json ) {
+		promise = promises[ url ] = get( url ).then( function ( json ) {
 			return JSON.parse( json );
 		});
+
+		// Pre-fetch next step
+		tutorialIndex = indexByTitle[ title ];
+		tutorialData = data.manifest[ tutorialIndex ];
+
+		if ( number < tutorialData.numSteps - 1 ) {
+			promise.then( () => fetch( title, number + 1 ) );
+		}
 	}
 
 	return promises[ url ];

7638904c0a7be4833878c3bc84beca2fd4107658
Rich-Harris <richard.a.harris@gmail.com>
2015-04-05 14:31:47 -0400

    use roadtrip for routing

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,7 @@
+import roadtrip from 'roadtrip';
 import AppView from './components/app';
 import Nav from './components/Nav';
 import getStep from 'getStep';
-import handleError from 'utils/handleError';
-import baseUrl from 'baseUrl';
 
 new Nav({
 	el: '.nav-container',
@@ -19,27 +18,14 @@ const view = new AppView({
 	}
 });
 
-// TODO use proper router?
-view.on({
-	navigate ( tutorialTitle, stepNumber ) {
-		var tutorialIndex, stepIndex;
-
-		getStep( tutorialTitle, stepNumber ).then( step => {
-			view.set( 'currentStep', step );
-			window.history.pushState( step, null, `${baseUrl}/${tutorialTitle}/${stepNumber}` );
-		}).catch( handleError );
-	}
-});
-
-// manage history
-if ( window.history && typeof window.history.replaceState === 'function' ) {
-	window.history.replaceState( data.step, '', window.location.pathname );
-}
-
-window.addEventListener( 'popstate', event => {
-	if ( event.state ) {
-		view.set( 'currentStep', event.state );
-	}
-});
+roadtrip
+	.add( '/:title/:step', {
+		enter ( route ) {
+			return getStep( route.params.title, route.params.step ).then( step => {
+				view.set( 'currentStep', step );
+			});
+		}
+	})
+	.start();
 
 window.view = view;
